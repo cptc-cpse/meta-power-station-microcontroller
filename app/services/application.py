547a4e84cdd16459fac50c8e41f2d9f0a4c5c62d
NullPointer1331 <130084942+NullPointer1331@users.noreply.github.com>
2026-05-28 12:30:28 -0700

    Created Application Service

---
+++ app/services/application.py
@@ -0,0 +1,20 @@
+import asyncio
+
+from app.configuration.config import Config
+from app.services.poller import PollerService
+from app.services.publication import PublicationService
+from app.models.power_reading import PowerReading
+
+class ApplicationService:
+    def __init__(self, config: Config):
+        self.SLEEP_INTERVAL_SECONDS = config.sleep_interval_seconds
+        self.poller_service = PollerService(config)
+        self.publication_service = PublicationService(config)
+
+    async def run_forever(self):
+        """Run the application indefinitely, polling for readings and publishing them."""
+        await self.poller_service.poller.setup_device()
+        while True:
+            readings = await self.poller_service.poll_to_readings()
+            self.publication_service.publish_readings(readings)
+            await asyncio.sleep(self.SLEEP_INTERVAL_SECONDS)
